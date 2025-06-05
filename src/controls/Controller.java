10e960a3f48ed6f2743c1db040ec38970506f580
LeonProgrammiert <leon.freudenmann@gmail.com>
2025-06-05 10:00:28 +0200

    Minor update to fit the "LookAndFeel" to the System-LookAndFeel

--- src/controls/Controller.java
+++ src/controls/Controller.java
@@ -5,12 +5,21 @@ import storage.DatabaseAccessor;
 import ui.Launcher;
 import ui.ListenEditor;
 
+import javax.swing.*;
 import java.io.File;
 import java.util.ArrayList;
 
 public class Controller {
 
     public static void main (String[] args) {
+
+        // Set look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         new Controller();
     }
 
@@ -27,9 +36,9 @@ public class Controller {
 
     }
 
-    public static void handleError(String localizedMessage) {
-        System.out.println(localizedMessage);
-
+    public static void handleError(String errorMessage) {
+        System.out.println(errorMessage);
+        // Später über JOptionPane ausgeben
     }
 
 
