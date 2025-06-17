4aaaf541c2efcc66ddfa62b88119582a97d10bd3
LeonProgrammiert <leon.freudenmann@gmail.com>
2025-06-17 14:02:17 +0200

    Merges

--- src/controls/Controller.java
+++ src/controls/Controller.java
@@ -2,14 +2,11 @@ package controls;
 
 import backend.CustomObject;
 import storage.DatabaseAccessor;
-import backend.CustomObject;
 import ui.ListEditor;
 import ui.Launcher;
 
-import javax.sound.sampled.*;
 import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
 
 public class Controller {
 
@@ -31,24 +28,13 @@ public class Controller {
     private final Launcher launcher;
     private final ListEditor listEditor;
 
-    private static Controller instance;
-
     public Controller() {
-        instance = this;
-
         databaseAccessor = new DatabaseAccessor();
         launcher = new Launcher(this);
         launcher.setVisible(true);
 
         listEditor = new ListEditor(this);
         listEditor.setVisible(false);
-    }
-
-    public static Controller getController() {
-        return instance;
-    }
-    public ListEditor getListEditor() {
-        return listEditor;
     }
 
     public static void handleError(String errorMessage) {
@@ -60,36 +46,18 @@ public class Controller {
         this.currentListName = name;
         databaseAccessor.addList(name);
         System.out.println("Neue Liste erstellt: " + name);
-        String path = System.getProperty("user.dir") + "/src/saves/" + name + ".csv";
-        openList(new File(path));
     }
 
     public void openList(File file) {
         CustomObject anker = databaseAccessor.openList(file);
 
         launcher.setVisible(false);
+        listEditor.setVisible(true);
         listEditor.openList(anker);
     }
 
     public void backToLauncher(CustomObject anker) {
         launcher.setVisible(true);
         listEditor.setVisible(false);
-    }
-
-    public void playSound(File soundFile) {
-        new Thread(() -> {
-            try {
-                AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioStream);
-                clip.start();
-
-                // Wait for the clip to finish playing
-                Thread.sleep(clip.getMicrosecondLength() / 1000);
-            } catch (UnsupportedAudioFileException | IOException | InterruptedException |
-                     LineUnavailableException e) {
-                handleError(e.getMessage());
-            }
-        }).start();
     }
 }
