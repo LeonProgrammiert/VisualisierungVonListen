877f818d2b91b257a47db0f86ac8136d04556bac
LeonProgrammiert <leon.freudenmann@gmail.com>
2025-07-02 17:22:08 +0200

    Deleted unnecessary todo

--- src/controls/Controller.java
+++ src/controls/Controller.java
@@ -146,7 +146,6 @@ public class Controller<T> {
         if (oldState == null) {
             displayMessage("Es gibt keinen vorherigen Zustand", "Fehlermeldung");
             return;
-            //TODO update unsavedChanges
         }
 
         // Displays the previous state in the editor
